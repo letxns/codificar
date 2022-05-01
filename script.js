936220fbfaad3f5f4d7a70af2a6342e152786517
letxns <lele_martins1999@yahoo.com.br>
2022-05-01 03:54:38 -0300

    primeiro commit

--- script.js
+++ script.js
@@ -18,3 +18,5 @@ codificar.addEventListener('click', function(){
 decodificar.addEventListener('click', function(){
     btnSubmit.innerText = "Decodificar";
 });
+
+//Projeto em que mensagens poderão ser codificadas e decodificadas através de duas formas: utilizando a cifra de César e a Base 64.
