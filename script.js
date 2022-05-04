85e47ebed04a9e3d76756e03dbe8025e708580f4
letxns <lele_martins1999@yahoo.com.br>
2022-05-04 00:57:49 -0300

    melhorias legibilidade do codigo e prevenir atualizacao da pagina

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 /*
 O que falta? 
-- implementar a função base64, que receberá uma variavel recebendo o valor inserido pelo usuário. 
-checar a opção marcada: code or decode? o valor do texto de saída vai receber a função com a lógica
-- implementar a função com a lógica, recebendo dois parâmetros: uma entrada e um numero. checar a opção marcada: code or decode?
-a função irá retornar, caso a opção esteja marcada: btoa(input) e, caso não: atob(input)
 -funcao cesar que receberá uma variavel recebendo o valor inserido pelo usuário. Além disso, utilizaremos o split('').
 variavel checando a opção marcada. variavel recebendo os passos (utilizar um parseInt recebendo o valor do campo de entrada).
 caso marcado codificar, entao o valor do campo resultado receberá a função codificando. se não, receberá a função decodificando.
 -função cesarCod, recebendo dois parametros(estudar melhor sobre eles)
-
-
-
-
-
-
 
 */
 
@@ -47,11 +37,10 @@ decodificar.addEventListener('click', function(){
     btnSubmit.innerText = "DECODIFICAR";
 });
 
-btn.addEventListener('click', function enviar(e){
-    e.preventDefault
+btn.addEventListener('click', function(e){
+    e.preventDefault() 
 });
 
-//é esse codigo q ta codificando ao selecionar o tipo de criptografia
 function base64(){
     if(codificar.checked){
         return output.value = btoa(input.value);
@@ -60,24 +49,19 @@ function base64(){
     }
 }
 
-//responsavel por codificar/decodificar
+function cifraCesar(){
+    if(codificar.checked){
+        //return output.value = btoa(input.value);
+    } else if(decodificar.checked){
+        //return output.value = atob(input.value);
+    }
+}
 
-
-function enviar(e){
+function enviar(){
     if(criptografia.value == "base64"){
-        base64()
-        btn.addEventListener('click', function base64(){
-            e.preventDefault() 
-        });
-        return base64()   
-    }else if(criptografia.value == "base64"){
-        base64()
-        e.preventDefault()
-    }else if(criptografia.value == "cifraCesar"){
-        cifraCesar()
+        base64() 
     }else if(criptografia.value == "cifraCesar"){
         cifraCesar()
     }
 
 }
-
