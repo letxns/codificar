29b397f699851b9a782eb753fe7c473a70bac885
letxns <lele_martins1999@yahoo.com.br>
2022-05-04 00:29:00 -0300

    implementação da logica da funcao base64

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-var selecionarCriptografia = document.querySelector('#tipoCodigo');
+/*
+O que falta? 
+- implementar a função base64, que receberá uma variavel recebendo o valor inserido pelo usuário. 
+checar a opção marcada: code or decode? o valor do texto de saída vai receber a função com a lógica
+- implementar a função com a lógica, recebendo dois parâmetros: uma entrada e um numero. checar a opção marcada: code or decode?
+a função irá retornar, caso a opção esteja marcada: btoa(input) e, caso não: atob(input)
+-funcao cesar que receberá uma variavel recebendo o valor inserido pelo usuário. Além disso, utilizaremos o split('').
+variavel checando a opção marcada. variavel recebendo os passos (utilizar um parseInt recebendo o valor do campo de entrada).
+caso marcado codificar, entao o valor do campo resultado receberá a função codificando. se não, receberá a função decodificando.
+-função cesarCod, recebendo dois parametros(estudar melhor sobre eles)
+
+
+
+
+
+
+
+*/
+
+var criptografia = document.querySelector('#tipoCodigo');
 var btn = document.querySelector('#btnSubmit');
+var codificar = document.querySelector('#codificar');
+var decodificar = document.querySelector('#decodificar');
+var input = document.querySelector('#textoEntrada');
+var output = document.querySelector('#textoSaida');
 //var radioButtons = document.getElementById('radioButtons');
 
 tipoCodigo.addEventListener('change', function(event){
     var tipoSelecionado = event.target.value;
+
     if(tipoSelecionado == "cifraCesar"){
         passos.style.display = "block";
         btnSubmit.setAttribute('click', cifraCesar());
@@ -21,13 +45,100 @@ codificar.addEventListener('click', function(){
 
 decodificar.addEventListener('click', function(){
     btnSubmit.innerText = "DECODIFICAR";
-    decodificar();
 });
 
-function cifraCesar(){
-    alert("OI CESAR");
+btn.addEventListener('click', function enviar(e){
+    e.preventDefault
+});
+
+//é esse codigo q ta codificando ao selecionar o tipo de criptografia
+function base64(){
+    if(codificar.checked){
+        return output.value = btoa(input.value);
+    } else if(decodificar.checked){
+        return output.value = atob(input.value);
+    }
 }
 
-function base64(){
-    alert("OI BASE64");
+//responsavel por codificar/decodificar
+
+
+function enviar(e){
+    if(criptografia.value == "base64"){
+        base64()
+        btn.addEventListener('click', function base64(){
+            e.preventDefault() 
+        });
+        return base64()   
+    }else if(criptografia.value == "base64"){
+        base64()
+        e.preventDefault()
+    }else if(criptografia.value == "cifraCesar"){
+        cifraCesar()
+    }else if(criptografia.value == "cifraCesar"){
+        cifraCesar()
+    }
+
 }
+
+/*
+
+
+codificar.addEventListener('click', function(){
+    btnSubmit.innerText = "CODIFICAR";
+});
+
+decodificar.addEventListener('click', function(){
+    btnSubmit.innerText = "DECODIFICAR";
+});
+
+
+-----------------------------------------------------------------------------------------------------
+
+codificar.addEventListener('click', function btnCodificar(){
+    btnSubmit.innerText = "CODIFICAR";
+    output.value = btoa(input.value);
+    //let input = btoa(input.value)
+   // let output = imput.value 
+});
+
+decodificar.addEventListener('click', function btnDecodificar(){
+    btnSubmit.innerText = "DECODIFICAR";
+    output.value = atob(input.value);
+});
+
+*/
+/*
+function base64(){
+    let codificar = input.value;
+    if(tipoSelecionado == "base64"){
+        btn.addEventListener('click', (e) =>{
+            input.value = btoa(output.value);
+            e.preventDefault()
+        });
+    }
+}*/
+
+/*
+function base64(){
+
+}
+
+decodificar.addEventListener('click', () =>{
+    output.value = atob(output.value);
+    preventDefault()
+});*/
+
+/*function cifraCesar(){
+    var codificar = ((codigoASC - 65 + passos) % 26 + 65);
+    return String.fromCharCode(codificar);
+}*/
+
+/*
+function base64(){
+    var criptografia = tipoCodigo.value
+    if(criptografia == "base64"){
+            output.value = btoa(output.value);
+    }
+}
+*/
