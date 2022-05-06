d88f56273b013d2bc9573f1d6dd97c44fb1cd1c0
letxns <lele_martins1999@yahoo.com.br>
2022-05-06 02:32:22 -0300

    logica da cifra de cesar e melhorias de layout

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
-/*
-O que falta? 
--funcao cesar que receberá uma variavel recebendo o valor inserido pelo usuário. Além disso, utilizaremos o split('').
-variavel checando a opção marcada. variavel recebendo os passos (utilizar um parseInt recebendo o valor do campo de entrada).
-caso marcado codificar, entao o valor do campo resultado receberá a função codificando. se não, receberá a função decodificando.
--função cesarCod, recebendo dois parametros(estudar melhor sobre eles)
-
-*/
-
 var criptografia = document.querySelector('#tipoCodigo');
 var btn = document.querySelector('#btnSubmit');
 var codificar = document.querySelector('#codificar');
 var decodificar = document.querySelector('#decodificar');
 var input = document.querySelector('#textoEntrada');
 var output = document.querySelector('#textoSaida');
-//var radioButtons = document.getElementById('radioButtons');
+var passos = document.querySelector('#passos');
 
 tipoCodigo.addEventListener('change', function(event){
     var tipoSelecionado = event.target.value;
-
     if(tipoSelecionado == "cifraCesar"){
-        passos.style.display = "block";
-        btnSubmit.setAttribute('click', cifraCesar());
+        passos.style.display = "flex";
     }else if(tipoSelecionado == "base64"){
         passos.style.display = "none";
-        btnSubmit.setAttribute('click', base64());
     } else{
         passos.style.display = "none";
     }
@@ -42,26 +30,53 @@ btn.addEventListener('click', function(e){
 });
 
 function base64(){
-    if(codificar.checked){
+    if(codificar.checked && tipoCodigo.value == "base64"){
         return output.value = btoa(input.value);
-    } else if(decodificar.checked){
+    } else if(decodificar.checked && tipoCodigo.value == "base64"){
         return output.value = atob(input.value);
     }
 }
 
 function cifraCesar(){
-    if(codificar.checked){
-        //return output.value = btoa(input.value);
-    } else if(decodificar.checked){
-        //return output.value = atob(input.value);
+    var input = input.value.split('');
+    var incremento =  parseInt(passos.value);
+    if (codificar.checked){
+        output.value = cesarCodifica(input, incremento);
+    } else if (decodificar.checked){
+        output.value = cesarDecodifica(input, incremento);
     }
+}
+
+function cesarCodifica(input, incremento){
+    return input.map((texto)=>{
+        var codifique = texto.charCodeAt()
+        if(codifique >= 65 && codifique <= 90){
+            return String.fromCharCode(((codifique - 65 + incremento) % 26) + 65);
+        }else if(codifique >= 97 && codifique <= 122){
+            return String.fromCharCode(((codifique - 97 + incremento) % 26) + 97);
+        }else {
+            return texto
+        }
+    }).join('')
+}
+
+function cesarDecodifica(input, incremento){
+    return input.map((texto)=>{
+        var codifique = texto.charCodeAt()
+        if(codifique >= 65 && codifique <= 90){
+            return String.fromCharCode(((codifique - 90 - incremento) % 26) + 90);
+        }else if(codifique >= 97 && codifique <= 122){
+            return String.fromCharCode(((codifique - 122 - incremento) % 26) + 122);
+        }else {
+            return texto
+        }
+    }).join('')
 }
 
 function enviar(){
     if(criptografia.value == "base64"){
-        base64() 
+        return base64() 
     }else if(criptografia.value == "cifraCesar"){
-        cifraCesar()
+        return cifraCesar()
     }
-
 }
